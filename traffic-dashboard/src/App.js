b39853e45350232a4243a8bfad6a6d03b747bc30
Karthik <24070243025@sig.ac.in>
2025-03-01 17:28:05 +0530

    Started making a dashboard

---
+++ traffic-dashboard/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Traffic & Accident Analysis Dashboard</h1>
+      <p>Welcome! Let's build an interactive web app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
